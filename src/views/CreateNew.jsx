d8311da980aae352b26edbd23b548d0913586319
Dylan <Dylanalexandermcdougall@gmail.com>
2023-04-22 16:00:01 -0400

    CreateNew centering

--- src/views/CreateNew.jsx
+++ src/views/CreateNew.jsx
@@ -9,32 +9,33 @@ function CreateNew() {
 
         <main className="flex flex-col">
             <div>
-                <div id="goback">
+                <div id="goback" className="card flex flex-col items-center">
                     <a href="list">&larr; My To Do List</a>
                 </div>
                 <form action="/list" className="m-auto">
-                    <label for="taskName">Task Name:</label> <br />
-                    <input type="text" name="taskName" placeholder="Untitled" /> <br />
-                    <label for="taskCategory">Category:</label> <br />
-                    <input type="text" name="taskCategory" /> <br />
-                    <label for="taskUrgency">Urgency:</label> <br />
+                    <div className="card flex flex-col items-center">
+                    <label for="taskName">Task Name:</label>
+                    <input type="text" name="taskName" placeholder="Untitled" />
+                    <label for="taskCategory">Category:</label>
+                    <input type="text" name="taskCategory" />
+                    <label for="taskUrgency">Urgency:</label>
                     <select name="taskUrgency">
                         <option value="verylow">Very Low</option>
                         <option value="low">Low</option>
                         <option value="normal" selected="selected">Normal</option>
                         <option value="high">High</option>
                         <option value="veryhigh">Very High</option>
-                    </select> <br />
-                    <label for="taskStartDate" >Start Date:</label> <br />
-                    <input type="date" name="taskStartDate" className="tdate" /> <br />
-                    <label for="taskEndDate">End Date:</label> <br />
-                    <input type="date" name="taskEndDate" className="tdate" /> <br />
-                    <label for="taskLocation">Location:</label> <br />
-                    <input type="text" name="taskLocation" id="taskLocationInput" /> <br />
-                    <label for="taskDescription">Description:</label> <br />
-                    <textarea name="taskDescription" rows="8"></textarea> <br />
+                    </select>
+                    <label for="taskStartDate" >Start Date:</label>
+                    <input type="date" name="taskStartDate" className="tdate" />
+                    <label for="taskEndDate">End Date:</label>
+                    <input type="date" name="taskEndDate" className="tdate" />
+                    <label for="taskLocation">Location:</label>
+                    <input type="text" name="taskLocation" id="taskLocationInput" />
+                    <label for="taskDescription">Description:</label>
+                    <textarea name="taskDescription" rows="8"></textarea>
+                    </div>
 
-                    <br />
                     <div className="flex">
                         <button type="submit" id="submit-button"
                                 className="m-auto hover:scale-125 transition-all shadow-lg">Add Task</button>
