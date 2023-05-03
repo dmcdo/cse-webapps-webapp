d11d7819a8824773a6f9d6fff3912025f78ccb75
Dylan <Dylanalexandermcdougall@gmail.com>
2023-05-03 16:19:20 -0400

    Add form check

--- src/views/CreateNew.jsx
+++ src/views/CreateNew.jsx
@@ -4,6 +4,32 @@ import Footer from './components/Footer';
 import "./stylesheets/createnew.css"
 
 function CreateNew() {
+    const submittask = (e) => {
+        e.preventDefault();
+        console.log(e);
+
+        let name        = document.getElementsByName("taskName")       [0].value;
+        let category    = document.getElementsByName("taskCategory")   [0].value;
+        let urgency     = document.getElementsByName("taskUrgency")    [0].value;
+        let startDate   = document.getElementsByName("taskStartDate")  [0].value;
+        let endDate     = document.getElementsByName("taskEndDate")    [0].value;
+        let location    = document.getElementsByName("taskLocation")   [0].value;
+        let description = document.getElementsByName("taskDescription")[0].value;
+
+        let missing = [];
+        if (!name)        missing.push("name");
+        if (!category)    missing.push("category");
+        if (!urgency)     missing.push("urgency");
+        if (!startDate)   missing.push("startDate");
+        if (!endDate)     missing.push("endDate");
+        if (!location)    missing.push("location");
+        if (!description) missing.push("description");
+        if (missing) {
+            alert("Please fill out the following fields before submitting:\n" + missing.join("\n"));
+            return;
+        }
+    };
+
     return <>
         <Header />
 
@@ -12,7 +38,7 @@ function CreateNew() {
                 <div id="goback" className="card flex flex-col items-center">
                     <a href="list">&larr; My To Do List</a>
                 </div>
-                <form action="/list" className="m-auto">
+                <form className="m-auto" onSubmit={submittask}>
                     <div className="card flex flex-col items-center">
                     <label for="taskName">Task Name:</label>
                     <input type="text" name="taskName" placeholder="Untitled" />
